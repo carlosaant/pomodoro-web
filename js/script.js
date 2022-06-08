d09a8de5f22c047ed2e82c99858ea25a08a9c2b4
Antonio Carlos <antc.minga@gmail.com>
2022-06-08 10:52:57 -0300

    feat: criado function onClick  para incrementar contador

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function incrementPom(elem) {
+  const elementoPai = document.getElementById(elem.parentNode.parentNode.id);
+  const elementoTemporizador = elementoPai.children[1].children[0];
+  console.log(elementoTemporizador);
+}
