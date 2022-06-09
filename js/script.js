d2d827ff38d30185c93e7b62925f8ed345b69f46
Antonio Carlos <antc.minga@gmail.com>
2022-06-09 09:46:53 -0300

    Feat: Criado função Incrementar contadores de acordo com a opçao selecionada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
+'use strict';
+
+//  CONFIGURAÇÕES
+// ------
+let temp_trabalho = 25;
+let temp_pausa = 5;
+let temp_sessoes = 3;
+//
+const incremento_trabalho = 5;
+const limite_minutos_trabalho = 60;
+const incremento_pausa = 1;
+const limite_minutos_pausa = 60;
+const incremento_sessoes = 1;
+const limite_sessoes = 10;
+// --------
+
+let campo_mostrador_trabalho = document.getElementById('tempo_trabalho');
+let campo_mostrador_pausa = document.getElementById('tempo_pausa');
+let campo_mostrador_sessoes = document.getElementById('tempo_sessao');
+
+onload = function () {
+  campo_mostrador_trabalho.value = temp_trabalho;
+  campo_mostrador_pausa.value = temp_pausa;
+  campo_mostrador_sessoes.value = temp_sessoes;
+};
+
 function incrementPom(elem) {
-  const elementoPai = document.getElementById(elem.parentNode.parentNode.id);
+  const elementoPai = elem.parentNode.parentNode;
   const elementoTemporizador = elementoPai.children[1].children[0];
-  console.log(elementoTemporizador);
+  if (elementoTemporizador.id === campo_mostrador_trabalho.id) {
+    const valueDoElemento = parseInt(elementoTemporizador.value);
+    if (valueDoElemento < limite_minutos_trabalho) {
+      elementoTemporizador.value = valueDoElemento + incremento_trabalho;
+    }
+  } else if (elementoTemporizador.id === campo_mostrador_pausa.id) {
+    const valueDoElemento = parseInt(elementoTemporizador.value);
+    if (valueDoElemento < limite_minutos_pausa) {
+      elementoTemporizador.value = valueDoElemento + incremento_pausa;
+    }
+  } else if (elementoTemporizador.id === campo_mostrador_sessoes.id) {
+    const valueDoElemento = parseInt(elementoTemporizador.value);
+    if (valueDoElemento < limite_sessoes) {
+      elementoTemporizador.value = valueDoElemento + incremento_sessoes;
+    }
+  }
 }
