e3da8fd53eb72f5942b7d2419f78d61f24dad9b2
Antonio Carlos <antc.minga@gmail.com>
2022-06-09 15:39:08 -0300

    refact: alterado name trabalho para foco

--- js/index.js
+++ js/index.js
@@ -2,24 +2,24 @@
 
 //  CONFIGURAÇÕES
 // ------
-let temp_trabalho = 25;
+let temp_foco = 25;
 let temp_pausa = 5;
 let temp_sessoes = 3;
 //
-const incremento_trabalho = 5;
-const limite_minutos_trabalho = 60;
+const incremento_foco = 5;
+const limite_minutos_foco = 60;
 const incremento_pausa = 1;
 const limite_minutos_pausa = 60;
 const incremento_sessoes = 1;
 const limite_sessoes = 10;
 // --------
 
-let campo_mostrador_trabalho = document.getElementById('tempo_trabalho');
+let campo_mostrador_foco = document.getElementById('tempo_foco');
 let campo_mostrador_pausa = document.getElementById('tempo_pausa');
 let campo_mostrador_sessoes = document.getElementById('tempo_sessao');
 
 onload = function () {
-  campo_mostrador_trabalho.value = temp_trabalho;
+  campo_mostrador_foco.value = temp_foco;
   campo_mostrador_pausa.value = temp_pausa;
   campo_mostrador_sessoes.value = temp_sessoes;
 };
@@ -27,10 +27,10 @@ onload = function () {
 function incrementPom(elem) {
   const elementoPai = elem.parentNode.parentNode;
   const elementoTemporizador = elementoPai.children[1].children[0];
-  if (elementoTemporizador.id === campo_mostrador_trabalho.id) {
+  if (elementoTemporizador.id === campo_mostrador_foco.id) {
     const valueDoElemento = parseInt(elementoTemporizador.value);
-    if (valueDoElemento < limite_minutos_trabalho) {
-      elementoTemporizador.value = valueDoElemento + incremento_trabalho;
+    if (valueDoElemento < limite_minutos_foco) {
+      elementoTemporizador.value = valueDoElemento + incremento_foco;
     }
   } else if (elementoTemporizador.id === campo_mostrador_pausa.id) {
     const valueDoElemento = parseInt(elementoTemporizador.value);
@@ -48,10 +48,10 @@ function incrementPom(elem) {
 function decrementPom(elem) {
   const elementoPai = elem.parentNode.parentNode;
   const elementoTemporizador = elementoPai.children[1].children[0];
-  if (elementoTemporizador.id === campo_mostrador_trabalho.id) {
+  if (elementoTemporizador.id === campo_mostrador_foco.id) {
     const valueDoElemento = parseInt(elementoTemporizador.value);
     if (valueDoElemento > 5) {
-      elementoTemporizador.value = valueDoElemento - incremento_trabalho;
+      elementoTemporizador.value = valueDoElemento - incremento_foco;
     }
   } else if (elementoTemporizador.id === campo_mostrador_pausa.id) {
     const valueDoElemento = parseInt(elementoTemporizador.value);
@@ -66,12 +66,12 @@ function decrementPom(elem) {
   }
 }
 
-campo_mostrador_trabalho.onchange = function () {
-  if (parseInt(campo_mostrador_trabalho.value) > limite_minutos_trabalho) {
-    campo_mostrador_trabalho.value = limite_minutos_trabalho;
+campo_mostrador_foco.onchange = function () {
+  if (parseInt(campo_mostrador_foco.value) > limite_minutos_foco) {
+    campo_mostrador_foco.value = limite_minutos_foco;
   }
-  if (parseInt(campo_mostrador_trabalho.value) < 0) {
-    campo_mostrador_trabalho.value = temp_trabalho;
+  if (parseInt(campo_mostrador_foco.value) < 0) {
+    campo_mostrador_foco.value = temp_foco;
   }
 };
 
