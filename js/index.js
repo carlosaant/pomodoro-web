40820f9fcabe78bdabf0ba6850f3db06e421595e
Antonio Carlos <antc.minga@gmail.com>
2022-06-10 16:07:36 -0300

    refact: alterado variaveis pomodo para objeto, modificado padding button continuar

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@
 
 //  CONFIGURAÇÕES
 // ------
-let temp_foco = 25;
-let temp_pausa = 5;
-let temp_sessoes = 3;
+let _pomodoro_app = {
+  temp_foco: 25,
+  temp_pausa: 5,
+  temp_sessoes: 3
+};
+
 //
 const incremento_foco = 5;
 const limite_minutos_foco = 60;
@@ -17,11 +20,14 @@ const limite_sessoes = 10;
 let campo_mostrador_foco = document.getElementById('tempo_foco');
 let campo_mostrador_pausa = document.getElementById('tempo_pausa');
 let campo_mostrador_sessoes = document.getElementById('tempo_sessao');
+let btn_continuar = document.getElementById('btnContinuar');
 
 onload = function () {
-  campo_mostrador_foco.value = temp_foco;
-  campo_mostrador_pausa.value = temp_pausa;
-  campo_mostrador_sessoes.value = temp_sessoes;
+  campo_mostrador_foco.value = _pomodoro_app.temp_foco;
+  campo_mostrador_pausa.value = _pomodoro_app.temp_pausa;
+  campo_mostrador_sessoes.value = _pomodoro_app.temp_sessoes;
+
+  btn_continuar.addEventListener('click', salvarDados);
 };
 
 function incrementPom(elem) {
@@ -71,7 +77,7 @@ campo_mostrador_foco.onchange = function () {
     campo_mostrador_foco.value = limite_minutos_foco;
   }
   if (parseInt(campo_mostrador_foco.value) < 0) {
-    campo_mostrador_foco.value = temp_foco;
+    campo_mostrador_foco.value = _pomodoro_app.temp_foco;
   }
 };
 
@@ -80,7 +86,7 @@ campo_mostrador_pausa.onchange = function () {
     campo_mostrador_pausa.value = limite_minutos_pausa;
   }
   if (parseInt(campo_mostrador_pausa.value) < 0) {
-    campo_mostrador_pausa.value = temp_pausa;
+    campo_mostrador_pausa.value = _pomodoro_app.temp_pausa;
   }
 };
 
@@ -89,6 +95,8 @@ campo_mostrador_sessoes.onchange = function () {
     campo_mostrador_sessoes.value = limite_sessoes;
   }
   if (parseInt(campo_mostrador_sessoes.value) < 0) {
-    campo_mostrador_sessoes.value = temp_sessoes;
+    campo_mostrador_sessoes.value = _pomodoro_app.temp_sessoes;
   }
 };
+
+function salvarDados() {}
