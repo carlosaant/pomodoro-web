f1a28702d0fbcc1b4b40c85f94e1f6c05387326f
Antonio Carlos <antc.minga@gmail.com>
2022-06-13 15:08:34 -0300

    refact: serializando e deserializando o _pomodoro._pomofoco para passar apenas o valor, e nao alterar o objeto

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -49,25 +49,44 @@ function setPausaModo() {
 // ------------
 
 function iniciarFocoPomodoro() {
-  if (!_pomodoro._pomofoco.active) {
-    _pomofoco.active = true;
-    timeInterval = setInterval(timerExibe(_pomofoco), 1000);
+  if (_pomodoro._pomosessoes > 0) {
+    if (!_pomodoro._pomofoco.active) {
+      _pomodoro._pomofoco.active = true;
+      _pomodoro._pomopausa.active = false;
+      // serializando e deserializando o _pomodoro._pomofoco para passar apenas o valor, e nao alterar o objeto em si
+      timeInterval = setInterval(
+        timerExibe(JSON.parse(JSON.stringify(_pomodoro._pomofoco)), 'foco'),
+        100
+      );
+    }
+  } else {
+    alert('sessoes encerradas!');
   }
 }
 
 function iniciarPausaPomodoro() {
   if (!_pomodoro._pomopausa.active) {
-    _pomopausa.active = true;
-    timeInterval = setInterval(timerExibe(_pomopausa), 1000);
+    _pomodoro._pomopausa.active = true;
+    _pomodoro._pomofoco.active = false;
+    _pomodoro._pomosessoes -= 1;
+    timeInterval = setInterval(
+      timerExibe(JSON.parse(JSON.stringify(_pomodoro._pomopausa)), 'pausa'),
+      100
+    );
   }
 }
 
-function timerExibe(_pomo_mostrador) {
+function timerExibe(_pomo_mostrador, tipo) {
   return function () {
     decrementaTimer(_pomo_mostrador);
     renderizaTimerTela(_pomo_mostrador);
     if (_pomo_mostrador.seg === 0 && _pomo_mostrador.min === 0) {
       clearInterval(timeInterval);
+      if (tipo === 'foco') {
+        setPausaModo();
+      } else {
+        setFocoModo();
+      }
     }
   };
 }
@@ -88,6 +107,7 @@ function decrementaTimer(_pomo_mostrador) {
     _pomo_mostrador.seg = 59;
     _pomo_mostrador.min--;
   } else _pomo_mostrador.seg--;
+  console.log(_pomo_mostrador);
 }
 
 //------- funcoes de apoio
